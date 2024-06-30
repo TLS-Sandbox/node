21bae04ca7d9ee15f1249337230cc121da1894b1
Jorge Feijoo <jorgefeijoom@gmail.com>
2024-06-30 13:09:29 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello vercel!");
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
